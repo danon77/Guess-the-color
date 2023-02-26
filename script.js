e8520d810cc69094e3962c5a7d813a83b37c3b59
Daniil Velev <119440660+danon77@users.noreply.github.com>
2023-02-26 13:49:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let colors = document.querySelectorAll(".guess");
 let backgrounds = [  "forestgreen",  "olivedrab",  "turquoise",  "steelblue",  "darkviolet",  "indigo",  "slategray",  "goldenrod",  "maroon",  "tomato",  "crimson",  "khaki",];
 
 function shuffle(array) {
-  // Функция перемешивает элементы массива в случайном порядке
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
@@ -14,20 +13,18 @@ function shuffle(array) {
 }
 
 function newRound() {
-  // Запускаем новый раунд
-  shuffle(backgrounds); // Перемешиваем массив backgrounds
-  color.innerText = backgrounds[0]; // Выбираем первый элемент массива как цвет, который нужно угадать
-  setColors(); // Обновляем цвета кнопок
+  shuffle(backgrounds); 
+  color.innerText = backgrounds[0]; 
+  setColors(); 
 }
 
 function setColors() {
-  // Задаем цвета кнопкам из массива backgrounds
   for (let i = 0; i < colors.length; i++) {
     colors[i].style.background = backgrounds[i];
   }
 }
 
-newRound(); // Запускаем первый раунд
+newRound(); 
 
 for (let i = 0; i < colors.length; i++) {
   colors[i].addEventListener("click", check);
@@ -38,19 +35,16 @@ function check() {
   console.log(counter);
 
   if (this.style.background === color.innerText) {
-    // Если правильно угадали цвет
     count++;
     score.innerText = count;
   } else {
-    // Если неправильно угадали цвет
     console.log("Wrong guess");
   }
 
   if (counter === 12) {
-    // Если закончились все раунды
     alert(count);
-    location.reload(); // Перезагружаем страницу, чтобы начать заново
+    location.reload(); 
   } else {
-    newRound(); // Запускаем новый раунд
+    newRound(); 
   }
 }
